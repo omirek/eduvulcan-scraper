a1fcf8a3f21068a80f27a66a5cefaa4761634a43
omirek <140943274+omirek@users.noreply.github.com>
2026-05-05 08:13:13 +0200

    Configure supabase client with options
    Add options to supabase client for realtime configuration.

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ const { createClient } = require('@supabase/supabase-js');
 
 const supabase = createClient(
   process.env.SUPABASE_URL,
-  process.env.SUPABASE_SERVICE_KEY
+  process.env.SUPABASE_SERVICE_KEY,
+  {
+    realtime: false
+  }
 );
 
 (async () => {
